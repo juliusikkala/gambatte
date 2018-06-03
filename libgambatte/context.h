18e554cb8ea79b997a078a1ffa6cbf2151576f82
Julius Ikkala <julius.ikkala@gmail.com>
2018-06-03 21:09:19 +0300

    Show game title on the component itself

--- libgambatte/context.h
+++ libgambatte/context.h
@@ -2,11 +2,21 @@
 #define CONTEXT_H
 #include "gambatte.h"
 
+#define AUDIO_BUFFER_LENGTH (35112+2046)
+#define AUDIO_BUFFER_SIZE (sizeof(int16_t)*2*AUDIO_BUFFER_LENGTH)
+
+#define VIDEO_BUFFER_SIZE (sizeof(uint32_t)*160*144)
+#define VIDEO_PITCH 160
+
 struct Context
 {
     gambatte::GB gb;
+    uint8_t* video_buffer;
+    int16_t* audio_buffer;
+
     uint8_t* rom_data;
     size_t rom_size;
+    char title[16];
 };
 
 extern struct Context* ctx;
--- libgambatte/interface.cpp
+++ libgambatte/interface.cpp
@@ -2,6 +2,7 @@
 #include <emscripten.h>
 #include <cstdlib>
 #include <cstdio>
+#include <cstring>
 
 using namespace gambatte;
 
@@ -13,8 +14,11 @@ EMSCRIPTEN_KEEPALIVE
 void gambatte_boot()
 {
     ctx = new Context();
+    ctx->video_buffer = (uint8_t*)malloc(VIDEO_BUFFER_SIZE);
+    ctx->audio_buffer = (int16_t*)malloc(AUDIO_BUFFER_SIZE);
     ctx->rom_data = nullptr;
     ctx->rom_size = 0;
+    memset(ctx->title, 0, sizeof(ctx->title));
 }
 
 EMSCRIPTEN_KEEPALIVE
@@ -27,11 +31,30 @@ uint8_t* gambatte_allocate_rom(size_t rom_size)
     return ctx->rom_data;
 }
 
+
+EMSCRIPTEN_KEEPALIVE
+uint8_t* gambatte_get_video_buffer()
+{
+    return ctx->video_buffer;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int16_t* gambatte_get_audio_buffer()
+{
+    return ctx->audio_buffer;
+}
+
+EMSCRIPTEN_KEEPALIVE
+char* gambatte_get_title()
+{
+    return ctx->title;
+}
+
 EMSCRIPTEN_KEEPALIVE
 int gambatte_upload_rom()
 {
     LoadRes res = ctx->gb.load("This text should not matter, at all.");
-    printf("Rom title: %s\n", ctx->gb.romTitle().c_str());
+    strcpy(ctx->title, ctx->gb.romTitle().c_str());
     return res;
 }
 
